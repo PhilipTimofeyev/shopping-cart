7acc3b746d9b2f46dc573ca632198f6f0fdb0956
Philip Timofeyev <philiptimofeyev@gmail.com>
2024-09-25 16:43:51 -0700

    Update style for shop and item cards

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import shopCSS from './shop.module.css'
+import styles from './shop.module.css'
 import { useState, useContext } from 'react';
 import { CartContext } from '../App';
 
@@ -20,15 +20,17 @@ export default function Item ({product}) {
 
 
 	return (
-		<div className={shopCSS.card}>
+		<div className={styles.card}>
 			<h2>{product.title}</h2>
 			<img src={product.image} alt={"placeholder text"} />
 			<details>{product.description}</details>
 			<h3>Price: ${product.price}</h3>
-			<div>
-			<label htmlFor="">Quantity: </label>
-			<input type="number" min="1" defaultValue="1" className={shopCSS.amountInput} onChange={e => setAmount(Number(e.target.value))}/>
-			<button onClick={() => addToCart(product)} >Add to Cart </button>
+			<div className={styles.addCart}>
+				<div>
+					<label htmlFor="">Quantity: </label>
+					<input type="number" min="1" defaultValue="1" className={styles.amountInput} onChange={e => setAmount(Number(e.target.value))}/>
+				</div>
+				<button className={styles.addCartBtn} onClick={() => addToCart(product)} >Add to Cart </button>
 			</div>
 		</div>
 		)

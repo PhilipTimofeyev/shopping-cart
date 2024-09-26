d85d2bd688f1e0dda540626954b864919604ecd4
Philip Timofeyev <philiptimofeyev@gmail.com>
2024-09-25 20:09:28 -0700

    Add ability to remove items from cart

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -6,15 +6,24 @@ import { CartContext } from '../../App';
 export default function CartItem({ product, amount }) {
     const { cart, setCart } = useContext(CartContext)
 
+    function handleClick() {
+        const newCart = {...cart}
+        delete newCart[product.id]
+
+        setCart(newCart)
+    }
+
     return (
         <div className={styles.cartCard}>
             <div>
                 <img src={product.image} alt={"placeholder text"} />
                 <h3>{product.title}</h3>
             </div>
+            
             <div>
                 <h3>Quantity: {amount}</h3>
                 <h3>Price: ${product.price}</h3>
+                <button onClick={handleClick}>Remove</button>
             </div>
         </div>
     )

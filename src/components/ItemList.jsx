71d26c674374fff5b7aa07b06af85b91dfba9ce9
Philip Timofeyev <philiptimofeyev@gmail.com>
2024-09-25 19:49:42 -0700

    Add API error catching

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,30 +1,41 @@
 import { React, useEffect, useState } from 'react'
 import Item from './Item'
-import shopCSS from './shop.module.css'
+import styles from './shop.module.css'
 
 export default function ItemList () {
-
 	const [products, setProducts] = useState([])
+	const [isLoading, setIsLoading] = useState(false)
+	const [error, setError] = useState(null)
 
 	useEffect(() => {
 		const dataFetch = async () => {
-			const data = await (
-				await fetch(
-					'https://fakestoreapi.com/products',
-				)
-			).json();
-			setProducts(data)
+			setIsLoading(true)
+			
+			try {
+				const response = await fetch('https://fakestoreapi.com/products')
+				if (!response.ok) {
+					throw new Error('Network response was not ok')
+				}
+				const jsonData = await response.json();
+				setProducts(jsonData);
+			} catch (err) {
+				setError(err);
+			} finally {
+				setIsLoading(false)
+			}
 		};
 		dataFetch()
 	}, [])
 
 	return (
 		<>
-			<div className={shopCSS.cardsContainer}>
-			{products.map(product => (
-				<div key = {product.id}>{<Item product={product}/>}</div>
-			))
-			}
+			{isLoading && <h2>Loading...</h2>}
+			{error && <h2 style={{ color: "red" }}>Error: {error.message}</h2>}
+			<div className={styles.cardsContainer}>
+				{products.map(product => (
+					<div key = {product.id}>{<Item product={product}/>}</div>
+				))
+				}
 			</div>
 		</>
 		)
